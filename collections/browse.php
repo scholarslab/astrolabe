e4f1f488620136639c18fa374474d0a641d266fd
Jeremy Boggs <jeremy@clioweb.org>
2013-12-12 13:14:43 -0500

    Collections browse markup and output.

--- collections/browse.php
+++ collections/browse.php
@@ -13,24 +13,31 @@ echo head(array('title'=>$pageTitle,'bodyid'=>'collections','bodyclass' => 'brow
 
         <h2><?php echo link_to_collection(); ?></h2>
 
-        <?php if ($description = metadata('collection', array('Dublin Core', 'Description'), array('snippet' => 150))): ?>
-        <div class="element">
-            <p class="element-text"><?php echo $description; echo link_to_collection('show more.');?></p>
+        <?php if ($description = snippet_by_word_count(metadata('collection', array('Dublin Core', 'Description')), 50)): ?>
+        <div class="description collection-description">
+            <div class="element-text"><?php echo $description; echo link_to_collection('show more.');?></div>
         </div>
         <?php endif; ?>
-        
-        <?php if ($collection->hasContributor()): ?>
-        <div class="element">
-            <h3><?php echo __('Contributors(s)'); ?></h3>
-            <div class="element-text">
-                <p><?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('all'=>true, 'delimiter'=>', ')); ?></p>
+
+        <div class="meta collection-meta">
+            <?php if ($collection->hasContributor()): ?>
+            <div class="element">
+                <h3><?php echo __('Contributors(s)'); ?></h3>
+                <div class="element-text">
+                    <p><?php echo metadata('collection', array('Dublin Core', 'Contributor'), array('all'=>true, 'delimiter'=>', ')); ?></p>
+                </div>
             </div>
+            <?php endif; ?>
+
+<p class="view-items-link">
+<?php echo link_to_items_browse(
+                __("Items"),
+                array('collection' => metadata('collection', 'id')),
+                array('title' => __('View the items in %s', strip_tags(metadata('collection', array('Dublin Core', 'Title')))))
+                ); ?></p>
+
+            <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
         </div>
-        <?php endif; ?>
-
-        <p class="view-items-link"><?php echo link_to_items_browse(__('View the items in %s', metadata('collection', array('Dublin Core', 'Title'))), array('collection' => metadata('collection', 'id'))); ?></p>
-
-        <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
 
     </div><!-- end class="collection" -->
     <?php endforeach; ?>
