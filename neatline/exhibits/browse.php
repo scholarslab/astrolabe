7a935e49074111600c7d80cf5987b8dd01f24c4e
Jeremy Boggs <jeremy@clioweb.org>
2013-12-12 13:21:34 -0500

    Adds custom Neatline exhibits browse.
    Includes a snippet of the exhibit narrative.

---
+++ neatline/exhibits/browse.php
@@ -0,0 +1,39 @@
+<?php
+
+$title = __('Neatline | Browse Exhibits');
+
+echo head(array(
+  'title' => $title,
+  'content_class' => 'neatline'
+)); ?>
+
+<div id="primary">
+
+  <?php echo flash(); ?>
+  <h1><?php echo $title; ?></h1>
+
+  <?php if (nl_exhibitsHaveBeenCreated()): ?>
+
+    <div class="pagination"><?php echo pagination_links(); ?></div>
+
+      <?php foreach (loop('NeatlineExhibit') as $e): ?>
+        <h2>
+          <?php echo nl_getExhibitLink(
+            $e, 'show', nl_getExhibitField('title'),
+            array('class' => 'neatline'), true
+          );?>
+        </h2>
+        <div class="neatline-exhibit-description"><?php echo snippet_by_word_count(nl_getExhibitField('narrative'), 20); ?>.<?php echo nl_getExhibitLink(
+            $e, 'show', 'Read more.',
+            array('class' => 'neatline'), true
+          );?>
+        </div>
+      <?php endforeach; ?>
+
+    <div class="pagination"><?php echo pagination_links(); ?></div>
+
+  <?php endif; ?>
+
+</div>
+
+<?php echo foot(); ?>
