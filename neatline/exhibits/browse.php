6a89a519fffd1e0a224d4040190d27599fe527b8
Jeremy Boggs <jeremy@clioweb.org>
2013-12-12 15:24:43 -0500

    Check if there's a narrative before displaying a Read More link.

--- neatline/exhibits/browse.php
+++ neatline/exhibits/browse.php
@@ -23,11 +23,9 @@ echo head(array(
             array('class' => 'neatline'), true
           );?>
         </h2>
-        <div class="neatline-exhibit-description"><?php echo snippet_by_word_count(nl_getExhibitField('narrative'), 20); ?>.<?php echo nl_getExhibitLink(
-            $e, 'show', 'Read more.',
-            array('class' => 'neatline'), true
-          );?>
-        </div>
+        <?php if ($description = snippet_by_word_count(nl_getExhibitField('narrative'), 20)) : ?>
+        <div class="neatline-exhibit-description"><?php echo $description . nl_getExhibitLink($e, 'show', 'Read more.', array('class' => 'neatline'), true); ?></div>
+        <?php endif; ?>
       <?php endforeach; ?>
 
     <div class="pagination"><?php echo pagination_links(); ?></div>
